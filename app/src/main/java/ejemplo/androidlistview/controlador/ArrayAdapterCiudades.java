9df90f56418d0a52ce51785da579ccb6a08f61e0
atlgl <atl_gl@hotmail.com>
2017-02-15 17:15:32 -0600

    ListView

---
+++ app/src/main/java/ejemplo/androidlistview/controlador/ArrayAdapterCiudades.java
@@ -0,0 +1,46 @@
+package ejemplo.androidlistview.controlador;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import ejemplo.androidlistview.R;
+import ejemplo.androidlistview.modelo.Ciudades;
+
+/**
+ * Created by Angelus on 15/02/2017.
+ */
+
+public class ArrayAdapterCiudades extends ArrayAdapter<Ciudades> {
+    List<Ciudades> ciudadesList;
+    Activity activity;
+
+    public ArrayAdapterCiudades(Activity context,List<Ciudades> ciudadesList) {
+        super(context, R.layout.item_ciudad,ciudadesList );
+        this.activity=context;
+        this.ciudadesList=ciudadesList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater=activity.getLayoutInflater();
+        View view=inflater.inflate(R.layout.item_ciudad,null);
+        ImageView imageView= (ImageView) view.findViewById(R.id.imgCiudad);
+        TextView txtciudad=(TextView) view.findViewById(R.id.txtCiudadNombre);
+        TextView txtdesc=(TextView) view.findViewById(R.id.txtCiudaddesc);
+
+        imageView.setImageResource(ciudadesList.get(position).getFoto());
+        txtciudad.setText(ciudadesList.get(position).getNombre());
+        txtdesc.setText(ciudadesList.get(position).getDescripcion());
+        return view;
+    }
+}
---
+++ app/src/main/java/ejemplo/androidlistview/modelo/Ciudades.java
@@ -0,0 +1,57 @@
+package ejemplo.androidlistview.modelo;
+
+/**
+ * Created by Angelus on 15/02/2017.
+ */
+
+public class Ciudades {
+
+    private int idCiudad;
+    private String nombre;
+    private String descripcion;
+    private int foto;
+
+    public Ciudades(int idCiudad, String nombre, String descripcion, int foto) {
+        this.idCiudad = idCiudad;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.foto = foto;
+    }
+
+    public Ciudades(int idCiudad, String nombre) {
+        this.idCiudad = idCiudad;
+        this.nombre = nombre;
+    }
+
+    public int getIdCiudad() {
+        return idCiudad;
+    }
+
+    public void setIdCiudad(int idCiudad) {
+        this.idCiudad = idCiudad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+}
